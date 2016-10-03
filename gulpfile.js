a92a5507c5fe8403f102808f4f659ad8f986695a
ushiboy <ushiboy.dev@gmail.com>
2016-10-03 22:44:27 +0900

    fix build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,44 @@
 const gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
   connect = require('connect'),
   serveStatic = require('serve-static'),
   historyApiFallback = require('connect-history-api-fallback'),
   path = require('path'),
-  distDir = path.join(__dirname, 'example');
+  distDir = path.join(__dirname, 'example'),
+  webpack = require('webpack'),
+  bundler = webpack({
+    entry: {
+      'hash': './example/hash.js',
+      'history': './example/history.js'
+    },
+    devtool: 'inline-source-map',
+    output: {
+      path: distDir,
+      filename: '[name]-bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        }
+      ]
+    }
+  });
 
+gulp.task('js', cb => {
+  bundler.run((err, stats) => {
+    if (err) {
+      throw new $.util.PluginError('webpack:build', err);
+    }
+    $.util.log('[webpack:build]', stats.toString({
+      colors: true,
+      chunkModules: false
+    }));
+    cb();
+  });
+});
 
 gulp.task('serve', () => {
   const port = process.env.PORT || 3000;
@@ -22,4 +56,8 @@ gulp.task('serve', () => {
     .listen(port);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('dev', ['js', 'serve'], () => {
+  gulp.watch('example/**/*.js', ['js']);
+});
+
+gulp.task('default', ['dev']);
