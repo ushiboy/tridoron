746413664ac4a4efbb41c7aa629ed46de3806faf
ushiboy <ushiboy.dev@gmail.com>
2018-09-11 21:49:19 +0900

    update babel

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'presets': [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
+  'env': {
+    'development': {
+      'presets': [
+        'power-assert'
+      ]
+    }
+  }
+}
